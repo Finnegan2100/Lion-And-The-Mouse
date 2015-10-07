0b9694148effaf1af7696a4d2cde39c6b8b78283
Ben <Finnegan2100@gmail.com>
2015-10-07 18:28:14 -0400

    Refactored collision.js

--- js/collision.js
+++ js/collision.js
@@ -6,9 +6,9 @@ function hitTestPoint(pointX, pointY, sprite) {
 
 function checkCollision() {
   
-    if (hitTestPoint(LION.mouseX,LION.mouseY,hotSpot1))  {
+	if (hitTestPoint(LION.mouseX,LION.mouseY,hotSpot1))  {
  
-		if (LION.mouseDown === true && LION.currentPage > 1 && LION.currentPage < 21 && LION.context.globalAlpha === 1) {
+		if (LION.mouseDown && LION.currentPage > 1 && LION.currentPage < 21 && LION.context.globalAlpha === 1) {
 			LION.pressedRight = true;
 			LION.context.drawImage(rightButtonPressedImage,rightButtonPressed.x,rightButtonPressed.y,rightButtonPressed.width,rightButtonPressed.height);
 		}
@@ -16,131 +16,112 @@ function checkCollision() {
   
 	if (hitTestPoint(LION.mouseX,LION.mouseY,hotSpot2))  {
  
-	    if (LION.mouseDown === true && LION.currentPage > 1 && LION.context.globalAlpha === 1) {
+	    if (LION.mouseDown && LION.currentPage > 1 && LION.context.globalAlpha === 1) {
 		    LION.pressedHome = true;
 		    LION.context.drawImage(endHomePressedImage,homePressed.x,homePressed.y,homePressed.width,homePressed.height);
 		} 
-  }
+	}
   
-  
-   if (hitTestPoint(LION.mouseX,LION.mouseY,hotSpot3))  {
+	if (hitTestPoint(LION.mouseX,LION.mouseY,hotSpot3))  {
  
-		if(LION.mouseDown === true && LION.currentPage === 1 && LION.moveLeft === false && LION.context.globalAlpha === 1)
-		  {
-		  LION.onPressed = true;
-		  LION.context.drawImage(narradorOnPressedImage,narradorOnPressed.x,narradorOnPressed.y,narradorOnPressed.width,narradorOnPressed.height);
-		  }
-		  if(LION.mouseUp === true && LION.currentPage === 1 && LION.moveLeft === false && LION.context.globalAlpha === 1)
-		  {
-		  LION.context.drawImage(narradorOnImage,narradorOn.x,narradorOn.y,narradorOn.width,narradorOn.height);
-		  }
-  }
+		if (LION.mouseDown && LION.currentPage === 1 && !LION.moveLeft && LION.context.globalAlpha === 1) {
+			LION.onPressed = true;
+			LION.context.drawImage(narradorOnPressedImage,narradorOnPressed.x,narradorOnPressed.y,narradorOnPressed.width,narradorOnPressed.height);
+		}
+		if (LION.mouseUp && LION.currentPage === 1 && !LION.moveLeft && LION.context.globalAlpha === 1) {
+			LION.context.drawImage(narradorOnImage,narradorOn.x,narradorOn.y,narradorOn.width,narradorOn.height);
+		}
+	}
   
-   if(hitTestPoint(LION.mouseX,LION.mouseY,hotSpot4))  {
+	if (hitTestPoint(LION.mouseX,LION.mouseY,hotSpot4))  {
  
-		  if(LION.mouseDown === true && LION.currentPage === 1 && LION.moveLeft === false && LION.context.globalAlpha === 1)
-		  {
-		  LION.offPressed = true;
-		  LION.context.drawImage(narradorOffPressedImage,narradorOffPressed.x,narradorOffPressed.y,narradorOffPressed.width,narradorOffPressed.height);
-		  }	 
-		  if(LION.mouseUp === true && LION.currentPage === 1 && LION.moveLeft === false)
-		  {
-		   LION.context.drawImage(narradorOffImage,narradorOff.x,narradorOff.y,narradorOff.width,narradorOff.height);
-		  }
-  }
+		if (LION.mouseDown && LION.currentPage === 1 && !LION.moveLeft && LION.context.globalAlpha === 1) {
+			LION.offPressed = true;
+			LION.context.drawImage(narradorOffPressedImage,narradorOffPressed.x,narradorOffPressed.y,narradorOffPressed.width,narradorOffPressed.height);
+		}	 
+		if (LION.mouseUp && LION.currentPage === 1 && !LION.moveLeft) {
+			LION.context.drawImage(narradorOffImage,narradorOff.x,narradorOff.y,narradorOff.width,narradorOff.height);
+		}
+	}
   
-  //console.log(pages[20].x);
-   if(hitTestPoint(LION.mouseX,LION.mouseY,hotSpot5))  {
+	if (hitTestPoint(LION.mouseX,LION.mouseY,hotSpot5))  {
 
-		  if(LION.mouseDown === true && LION.currentPage === 21)
-		  {
-		  startFade = true;
-		  LION.value1 = 0;
-		  }
-  }
+		if (LION.mouseDown && LION.currentPage === 21) {
+			startFade = true;
+			LION.value1 = 0;
+		}
+	}
   
-     if(hitTestPoint(LION.mouseX,LION.mouseY,hotSpot6))  {
+	if (hitTestPoint(LION.mouseX,LION.mouseY,hotSpot6))  {
  
-	     if(LION.mouseDown === true)
-		  {
-		  //window.focus();
-		  window.close();
-		  }
-  }
+		if (LION.mouseDown) {
+			window.close();
+		}
+	}
   
-   
-  if(hitTestPoint(LION.tapX,LION.tapY,hotSpot1))  {
+	if (hitTestPoint(LION.tapX,LION.tapY,hotSpot1))  {
  
-	     if(LION.touchDown === true && LION.currentPage > 1 && LION.currentPage < 21 && startFade === false && LION.context.globalAlpha === 1)
-		  {
-		  LION.pressedRight = true;
-		  LION.context.drawImage(rightButtonPressedImage,rightButtonPressed.x,rightButtonPressed.y,rightButtonPressed.width,rightButtonPressed.height);
-		  }
-  }
+		if (LION.touchDown && LION.currentPage > 1 && LION.currentPage < 21 && !startFade && LION.context.globalAlpha === 1) {
+			LION.pressedRight = true;
+			LION.context.drawImage(rightButtonPressedImage,rightButtonPressed.x,rightButtonPressed.y,rightButtonPressed.width,rightButtonPressed.height);
+		}
+	}
   
-  if(hitTestPoint(LION.tapX,LION.tapY,hotSpot2))  {
+	if (hitTestPoint(LION.tapX,LION.tapY,hotSpot2))  {
  
-	      if(LION.touchDown === true && LION.currentPage > 1 && LION.context.globalAlpha === 1)
-		  {
-		  LION.pressedHome = true;
-		  LION.context.drawImage(endHomePressedImage,homePressed.x,homePressed.y,homePressed.width,homePressed.height);
-		  LION.value1 = 0;
-		  } 
-  }
+		if (LION.touchDown && LION.currentPage > 1 && LION.context.globalAlpha === 1) {
+			LION.pressedHome = true;
+			LION.context.drawImage(endHomePressedImage,homePressed.x,homePressed.y,homePressed.width,homePressed.height);
+			LION.value1 = 0;
+		} 
+	}
   
-  
-    if(hitTestPoint(LION.tapX,LION.tapY,hotSpot3))  {
+	if (hitTestPoint(LION.tapX,LION.tapY,hotSpot3))  {
  
-		  if(LION.touchDown === true && LION.currentPage === 1 && LION.moveLeft === false && LION.context.globalAlpha === 1)
-		  {
-		  LION.onPressed = true;
-		  LION.context.drawImage(narradorOnPressedImage,narradorOnPressed.x,narradorOnPressed.y,narradorOnPressed.width,narradorOnPressed.height);
-		  }
-		  if(LION.touchUp === true && LION.currentPage === 1 && LION.moveLeft === false && LION.context.globalAlpha === 1)
-		  {
-		  LION.context.drawImage(narradorOnImage,narradorOn.x,narradorOn.y,narradorOn.width,narradorOn.height);
-		  }
-  }
+		if (LION.touchDown && LION.currentPage === 1 && !LION.moveLeft && LION.context.globalAlpha === 1) {
+			LION.onPressed = true;
+			LION.context.drawImage(narradorOnPressedImage,narradorOnPressed.x,narradorOnPressed.y,narradorOnPressed.width,narradorOnPressed.height);
+		}
+		if (LION.touchUp && LION.currentPage === 1 && !LION.moveLeft && LION.context.globalAlpha === 1) {
+			LION.context.drawImage(narradorOnImage,narradorOn.x,narradorOn.y,narradorOn.width,narradorOn.height);
+		}
+	}
   
-   if(hitTestPoint(LION.tapX,LION.tapY,hotSpot4))  {
+	if (hitTestPoint(LION.tapX,LION.tapY,hotSpot4))  {
  
-		  if(LION.touchDown === true && LION.currentPage === 1 && LION.moveLeft === false && LION.context.globalAlpha === 1)
-		  {
-		  LION.offPressed = true;
-		  LION.context.drawImage(narradorOffPressedImage,narradorOffPressed.x,narradorOffPressed.y,narradorOffPressed.width,narradorOffPressed.height);
-		  }	 
-		  if(LION.touchUp === true && LION.currentPage === 1 && LION.moveLeft === false && LION.context.globalAlpha === 1)
-		  {
-		   LION.context.drawImage(narradorOffImage,narradorOff.x,narradorOff.y,narradorOff.width,narradorOff.height);
-		  }
-  }
+		if(LION.touchDown && LION.currentPage === 1 && !LION.moveLeft && LION.context.globalAlpha === 1) {
+			LION.offPressed = true;
+			LION.context.drawImage(narradorOffPressedImage,narradorOffPressed.x,narradorOffPressed.y,narradorOffPressed.width,narradorOffPressed.height);
+		}	 
+		if (LION.touchUp && LION.currentPage === 1 && !LION.moveLeft && LION.context.globalAlpha === 1) {
+			LION.context.drawImage(narradorOffImage,narradorOff.x,narradorOff.y,narradorOff.width,narradorOff.height);
+		}
+	}
   
-   if(hitTestPoint(LION.tapX,LION.tapY,hotSpot5))  {
-	//mike
-	      if(LION.touchDown === true && LION.currentPage === 21 && LION.context.globalAlpha === 1)
-		  {
-		  startFade = true;
-		  LION.value1 = 0;
-		  }
-  }
+	if (hitTestPoint(LION.tapX,LION.tapY,hotSpot5))  {
+
+		if (LION.touchDown && LION.currentPage === 21 && LION.context.globalAlpha === 1) {
+			startFade = true;
+			LION.value1 = 0;
+		}
+    }
   
-     if (hitTestPoint(LION.tapX,LION.tapY,hotSpot6))  {
+	if (hitTestPoint(LION.tapX,LION.tapY,hotSpot6))  {
  
-	      if (LION.touchDown === true && LION.currentPage > 1 && startFade === false && LION.context.globalAlpha === 1) {
+		if (LION.touchDown && LION.currentPage > 1 && !startFade && LION.context.globalAlpha === 1) {
 			window.history.back();
 			window.close();
 		 
-				if (LION.ios) {
-					window.history.back();
-					window.open('','_self').close();
-					window.close();
-					LION.touchDown = false;
-				}
+		if (LION.ios) {
+			window.history.back();
+			window.open('','_self').close();
+			window.close();
 			LION.touchDown = false;
-		  }
 		}
- 
+		LION.touchDown = false;
+		}
 	}
+}
 
 
   
